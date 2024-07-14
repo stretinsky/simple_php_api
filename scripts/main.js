8042258f9dbbc54ad9e97eb8c96535567a6d3ef5
stretinsky <danils0393@gmail.com>
2024-07-14 20:57:09 +0300

    + dz

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 function sendData() {
-    fetch("api/v2/text_processor.php", {
+    fetch("api/v/text_processor.php", {
         method: "POST",
         body: JSON.stringify({
             "text": document.getElementById('text').value,
-            // "reverse": document.getElementById('reverse').checked,
+            "reverse": document.getElementById('reverse').checked,
             "to_upper": document.getElementById('to-upper').checked
         }),
         headers: {
@@ -12,7 +12,6 @@ function sendData() {
     })
         .then((response) => response.json())
         .then((json) => {
-            console.log(json);
             document.getElementById('result').value = json.data.result
         });
 }
